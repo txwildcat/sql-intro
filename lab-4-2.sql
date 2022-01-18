cb18147ff071942797d31b803525f712fff1e9fb
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-01-18 04:57:28 +0000

    AFTER CLASS WORK 1.17

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -25,3 +25,9 @@
 -- | Ian        | Happ      |
 
 
+SELECT players.first_name, players.last_name
+FROM stats
+  INNER JOIN teams ON teams.id = stats.team_id
+  INNER JOIN players ON players.id = stats.player_id
+WHERE teams.year = 2020
+AND teams.name = "St. Louis Cardinals";
